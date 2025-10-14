346dca5a77043ce3a36093cd88a47994b61dd34a
Youssef Outahar <y.outahar@aidra.tech>
2025-10-14 19:50:49 +0100

    fix: update server port to 3000 in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { createServer } from "./server";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    port: 3000,
     fs: {
       allow: ["./client", "./shared"],
       deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
